39df8984e35656b86bc3f6c6c88a3295cd2ffcab
Kyle Inzunza <inzukyle@gmail.com>
2019-02-28 20:04:40 -0500

    Created and commented start of arduino code to read from load cel

--- LoadCellRead/LoadCellRead.ino
+++ LoadCellRead/LoadCellRead.ino
@@ -1,11 +1,31 @@
-int analogPin = 3;
-int val = 0;
-void setup() {
-  Serial.begin(9600);
+/*
+ * LoadCellRead
+ * Created for STORQUE and open source educational project
+ * 
+ * Team 
+ * 
+*/
 
+//pin to read from load cell
+const int sensorPin = A0;
+//value that will be read from load cell
+int sensorValue = 0;
+
+void setup() {
+
+  //set pin to input
+  pinMode(sensorPin, INPUT);
+
+  //set baud rate to 9600
+  Serial.begin(9600);
 }
 
 void loop() {
-  val = analogRead(analogPin);
-  Serial.write("Value" + val);
+
+  //delay by 10 milliseconds
+  delay(10);
+
+  //read voltage from pin and output to serial
+  sensorValue = analogRead(sensorPin);
+  Serial.write(sensorValue);
 }
