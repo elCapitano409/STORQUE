63c5a9c5119e10c0b026c41ab7e42831c3b380e0
Kyle Inzunza <inzukyle@gmail.com>
2019-03-20 23:20:49 -0400

    Added calibration function

--- LoadCellRead/LoadCellRead_AMP/LoadCellRead_AMP.ino
+++ LoadCellRead/LoadCellRead_AMP/LoadCellRead_AMP.ino
@@ -7,31 +7,46 @@
  * 
 */
 
-//declare analog pin to read value
+//amplification pin
 const int inPin = A0;
+//calibration pin 
+const int btPin = 3;
 //amount of times the load cell is checked per second
-const int sampleRate = 500;
+const int sampleRate = 1000;
+//calibration offset
+int offset = 0;
 
 void setup() {
 
-  char logo[] = "███████╗████████╗ ██████╗ ██████╗  ██████╗ ██╗   ██╗███████╗/n██╔════╝╚══██╔══╝██╔═══██╗██╔══██╗██╔═══██╗██║   ██║██╔════╝/n███████╗   ██║   ██║   ██║██████╔╝██║   ██║██║   ██║█████╗  /n╚════██║   ██║   ██║   ██║██╔══██╗██║▄▄ ██║██║   ██║██╔══╝  /n███████║   ██║   ╚██████╔╝██║  ██║╚██████╔╝╚██████╔╝███████╗/n╚══════╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝ ╚══▀▀═╝  ╚═════╝ ╚══════╝";
   
   //set input pin to input mode
-  pinMode(A0, INPUT);
+  pinMode(inPin, INPUT);
+
+  //set pullup pin
+  pinMode(btPin, INPUT_PULLUP);
 
   //set baud rate
   //must be same rate that serial monitor is running
   Serial.begin(9600);
 
-  //output team logo
   //if nothing is output to serial monitor then there is problem with arduino communicating with computer
-  Serial.println(logo);
 }
 
 void loop() {
+
+  //value of calibration pin
+  int calCheck;
+  //read from button pin
+  calCheck = digitalRead(btPin);
+
+  //if calibration button is pressed
+  if(calCheck == 0){
+    offset = analogRead(inPin);
+  }
+  
   //delay by sample period
   delay(1000/sampleRate);
 
-  //read the raw value and output to computer
-  Serial.println(analogRead(inPin));
+  //output offset value
+  Serial.println(analogRead(inPin) - offset);
 }
